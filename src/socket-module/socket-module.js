6f5140023e7e2b3ee3e706e9b8c4cfb3caa9827e
Perice Pope <perice09@gmail.com>
2019-10-13 02:21:00 -0700

    3001 socket-module  deploy

--- src/socket-module/socket-module.js
+++ src/socket-module/socket-module.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:4000');
+const socket = openSocket('http://localhost:3001');
 
 function attachListeners() {
     socket.on('music', (data) => {
