72546f6d2f467b0db2ebbaf2214784b3f7d91f4d
Perice Pope <perice09@gmail.com>
2019-10-13 02:16:54 -0700

    3001 deploy

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/*', function(req, res) {
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3001;
 
 server.listen(port, function() {
   console.log(`Express app running on port ${port}`)
